1642abbf2cc7ac04fb2e181b732cd522007305b1
optimalisatie <info@optimalisatie.nl>
2020-08-03 21:01:44 +0000

    ngx-keyval

--- test/specs/tests.js
+++ test/specs/tests.js
@@ -25,7 +25,7 @@ describe('Nginx key/val client tests', async function() {
 
         // load key/val store
         NKV = new NgxKeyVal({
-            "server": "http://kv.style.tools/"
+            "server": "http://localhost/"
         });
 
         assert.equal(typeof NKV, 'object');
